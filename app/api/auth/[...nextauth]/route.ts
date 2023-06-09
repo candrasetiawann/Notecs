0a3e5720f6460ce96133003e4615016ecaf0ecac
Candra Setiawan <candra12305@gmail.com>
2023-06-09 13:32:26 +0700

    add callback

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,54 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import NextAuth, { Profile } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const authOptions = {
-  adapter : PrismaAdapter(prisma),
-  providers : [
+  adapter: PrismaAdapter(prisma),
+  providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
-  secret: process.env.NEXTAUTH_SECRET
-}
+  callback: {
+    async signIn({ profile }: any) {
+      const email = profile.email;
+      //search user in database
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
 
-const handler = NextAuth(authOptions)
+      if (existingUser) {
+        return true;
+      } else {
+        //create new user
+        await prisma.user.create({
+          data: {
+            email: email,
+            name: profile.name,
+          },
+        });
+      }
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/components/auth/signin",
+  },
+};
 
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
