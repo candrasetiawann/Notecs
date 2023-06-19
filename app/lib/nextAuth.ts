60e97c3000c6b3b0ab98baff57c8d09783f1bd18
Candra Setiawan <candra12305@gmail.com>
2023-06-19 20:04:31 +0700

    move authOptions to @/app/lib/nextAuth

---
+++ app/lib/nextAuth.ts
@@ -0,0 +1,61 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import prisma from "./prisma";
+
+export const authOptions:AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile}: any) {
+      const email = profile.email;
+      //search user in database
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+
+      if (existingUser) {
+        return true;
+      } else {
+        //create new user
+        const newUser = await prisma.user.create({
+          data: {
+            email: email,
+            name: profile.name,
+            image: profile.image,
+            emailVerified:
+              profile.emailVerified !== null
+                ? profile.emailVerified
+                : undefined, // Set emailVerified to false
+          },
+        });
+
+        if (newUser) {
+          return true;
+        } else {
+          throw new Error("Failed to create new user");
+        }
+      }
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
