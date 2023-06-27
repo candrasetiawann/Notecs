2f771c48cb10ad20470ea741e8dcc0c17876db73
CandraSetiawan <candra12305@gmail.com>
2023-06-27 15:03:11 +0700

    fix route.ts

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,88 +1,8 @@
-// import prisma from "@/app/lib/prisma";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export const GET = async (req: NextRequest) => {
-//   const todos = await prisma.nextjs13todo.findMany({});
-//   return NextResponse.json({ todos });
-// };
-
-// export const POST = async (req: NextRequest) => {
-//   const { title, content } = await req.json();
-
-//   // const todos = await prisma.nextjs13todo.create({
-//   //   data: {
-//   //     title,
-//   //     content,
-//   //   },
-//   // });
-//   // return NextResponse.json({ todos });
-// };
-
-// export const DELETE = async (req: NextRequest) => {
-//   const url = new URL(req.url).searchParams;
-//   const id = Number(url.get("id")) || 0;
-//   console.log("id" + id);
-
-//   const todos = await prisma.nextjs13todo.delete({
-//     where: {
-//       id: id,
-//     },
-//   });
-
-//   if (!todos) {
-//     return NextResponse.json({ message: "error" }, { status: 500 });
-//   }
-//   return NextResponse.json({ message: "ok" }, { status: 200 });
-// };
-
-// export const PUT = async (req: NextRequest) => {
-//   const { id, title, content } = await req.json();
-
-//   const todos = await prisma.nextjs13todo.update({
-//     where: {
-//       id: Number(id),
-//     },
-//     data: {
-//       title,
-//       content,
-//     },
-//   });
-//   return NextResponse.json({ todos });
-// };
 
 import prisma from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
-
-export const GET = async (req: NextRequest, res: NextResponse) => {
-  const session = await getServerSession(authOptions);
-  console.log("ini session :", session);
-  if (!session) {
-    console.log("not authenticated");
-    return NextResponse.json({ authenticated: !!session, session });
-  }
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: session?.user?.email ?? "",
-    },
-  });
-
-  console.log("user : ", user);
-
-  if (user === null) {
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
-  }
-
-  const todos = await prisma.nextjs13todo.findMany({
-    where:{
-      authorId:user.id
-    }
-  });
-  console.log('ini todos : ',todos)
-  return NextResponse.json({ todos });
-};
+import { authOptions } from "@/app/lib/nextAuth";
 
 
 //POST
@@ -95,7 +15,7 @@ export const POST = async (req: NextRequest) => {
 
   //if user is missing
   if (!findUser) {
-    throw new Error("User ID is missing");
+    throw new Error("User is missing");
   }
 
   if (req.method === "POST") {
@@ -143,6 +63,7 @@ export const DELETE = async (req: NextRequest) => {
   return NextResponse.json({ message: "ok" }, { status: 200 });
 };
 
+
 //PUT
 export const PUT = async (req: NextRequest) => {
   const { id, title, content } = await req.json();
@@ -158,6 +79,8 @@ export const PUT = async (req: NextRequest) => {
   });
   return NextResponse.json({ todos });
 };
+
+
 
 //tes connection
 async function testDatabaseConnection() {
